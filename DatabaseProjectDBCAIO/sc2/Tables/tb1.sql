9474471e6e413a5a52bdaf6326aba49ad0b2d4d5
Caio Gabriel <CaioGabriel10@users.noreply.github.com>
2021-09-02 15:26:16 -0300

    Erro induzido unique constraint

--- DatabaseProjectDBCAIO/sc2/Tables/tb1.sql
+++ DatabaseProjectDBCAIO/sc2/Tables/tb1.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [sc2].[tb1] (
     [id]   INT          IDENTITY (1, 1) NOT NULL,
-    [nome] VARCHAR (50) NOT NULL,
+    [nome] VARCHAR (50) NOT NULL UNIQUE,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
 

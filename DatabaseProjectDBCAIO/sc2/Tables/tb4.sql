6f0f64749412844ef6e150dac6d400c0f5ee02bc
Caio Gabriel <CaioGabriel10@users.noreply.github.com>
2021-09-02 14:55:08 -0300

    Corrigindo schema tb3

--- DatabaseProjectDBCAIO/sc2/Tables/tb4.sql
+++ DatabaseProjectDBCAIO/sc2/Tables/tb4.sql
@@ -2,5 +2,5 @@ CREATE TABLE [dbo].[tb4]
 (
   [id_tb3] INT NOT NULL,
   valor INT NOT NULL UNIQUE,
-  CONSTRAINT FK_tb4_id_tdb3 FOREIGN KEY (id_tb3) REFERENCES [sc2].[tb3](Id)
+  CONSTRAINT FK_tb4_id_tdb3 FOREIGN KEY (id_tb3) REFERENCES [dbo].[tb3](Id)
 )
